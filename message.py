9c7745f96c8a5d805b7cd13f0ee1181cd7afa2c7
abhay <abhayrajsvm@gmail.com>
2026-09-18 15:22:28 +0530

    AIMessage,HumanMessage,SystemMEssagae through langchain.core_message added for storing the memory more efficiently

---
+++ message.py
@@ -0,0 +1,27 @@
+from langchain_core.messages import SystemMessage,HumanMessage,AIMessage
+from langchain_huggingface import ChatHuggingFace ,HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+print("Abhay")
+
+llm=HuggingFaceEndpoint(
+    repo_id="openai/gpt-oss-120b",
+    task="text-generation",
+    provider="auto",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+)
+model=ChatHuggingFace(llm=llm)
+
+message=[
+    SystemMessage(content="you are a helping assistent")
+]
+while True:
+    user_input=input("You :")
+    message.append(HumanMessage(content=user_input))
+    if user_input=="exit":
+        break
+    result=model.invoke(message)
+    message.append(AIMessage(content=result.content))
+    print(result.content)
+
+print(message)    
