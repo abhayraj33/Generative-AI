8f5afa130dd4b3a24a562544634b343b87dc1ae3
abhay <abhayrajsvm@gmail.com>
2026-09-18 13:35:59 +0530

    Chatbot added

---
+++ Chat.py
@@ -0,0 +1,21 @@
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+llm=HuggingFaceEndpoint(
+    repo_id="openai/gpt-oss-120b",
+    task="text-generation",
+    provider="auto",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+)
+
+model=ChatHuggingFace(llm=llm)
+while True:
+    user_input=input("You :")
+    if user_input=="exit":
+        break
+
+    result=model.invoke(user_input)
+    print("AI : ",result.content)
