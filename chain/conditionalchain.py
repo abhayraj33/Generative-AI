5d2f2ecf30845e24e53fd8d39f78b263e74fd8e5
abhay <abhayrajsvm@gmail.com>
2026-09-23 23:59:57 +0530

    contitional added

---
+++ chain/conditionalchain.py
@@ -0,0 +1,39 @@
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableBranch,RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm=HuggingFaceEndpoint(
+    repo_id="openai/gpt-oss-120b",
+    task="text-generation",
+    provider="auto",
+)
+
+model=ChatHuggingFace(llm=llm)
+
+parser=StrOutputParser()
+
+prompt1=PromptTemplate(
+    template="make a sentiment analyse and tell wheter it is a negative or positive do not give anythin insted of negative or positive i wanted to reveive only either negative or positive nothing else text as a output \n {feedback}"
+)
+
+prompt2=PromptTemplate(
+    template="write a message for the positive feedback  of our product or service \n{feedback} "
+)
+
+prompt3=PromptTemplate(
+    template="write a message for the negative feedback  of our product or service \n{feedback} "
+)
+
+feedback="today i went to the burgerking outlet with my mom and she was very to be there thanks to the burger king"
+
+chain=prompt1 | model | parser
+
+result=chain.invoke({"feedback":feedback})
+
+print(result)
+
+
