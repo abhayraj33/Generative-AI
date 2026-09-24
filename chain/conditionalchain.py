47850308a68fce9359c7544a085fe9b2d2c08a1a
abhay <abhayrajsvm@gmail.com>
2026-09-24 10:30:58 +0530

    RunnableBranch Addition

--- chain/conditionalchain.py
+++ chain/conditionalchain.py
@@ -3,6 +3,9 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableBranch,RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel,Field
+from typing import Literal
 
 load_dotenv()
 
@@ -16,9 +19,15 @@ model=ChatHuggingFace(llm=llm)
 
 parser=StrOutputParser()
 
+class feedback(BaseModel):
+    sentimet:Literal["positive","negative"]=Field(description="Give the sentiment of the feebback")
+
+parser2=PydanticOutputParser(pydantic_object=feedback)
+
 prompt1=PromptTemplate(
-    template="make a sentiment analyse and tell wheter it is a negative or positive do not give anythin insted of negative or positive i wanted to reveive only either negative or positive nothing else text as a output \n {feedback}",
-    input_variables=["feedback"]
+    template="make a sentiment analyse and tell wheter it is a negative or positive do not give  \n {feedback} \n {format_instruction}",
+    input_variables=["feedback"],
+    partial_variables={"format_instruction":parser2.get_format_instructions()}
 )
 
 prompt2=PromptTemplate(
@@ -33,10 +42,18 @@ prompt3=PromptTemplate(
 
 feedback="today i went to the burgerking outlet with my mom and she was very to be there thanks to the burger king"
 
-chain=prompt1 | model | parser
+clasifier_chain=prompt1 | model | parser2
+
+branch_Chain=RunnableBranch(
+    (lambda x :x.sentimet=="positive", prompt2 | model | parser),
+    (lambda x :x.sentimet=="negative", prompt3 | model | parser),
+    RunnableLambda(lambda x: "Sentimentc ould not not found")
+)
+
+chain=clasifier_chain | branch_Chain
 
 result=chain.invoke({"feedback":feedback})
 
 print(result)
 
-
+chain.get_graph().print_ascii()
