42ed2fd3b70c0e21520631d034e2d7fa864f4546
abhay <abhayrajsvm@gmail.com>
2026-09-24 14:35:09 +0530

    updated version

--- self_buillt_project/review_Genrate_and_give_sentiment.py
+++ self_buillt_project/review_Genrate_and_give_sentiment.py
@@ -10,26 +10,26 @@ load_dotenv()
 llm=HuggingFaceEndpoint(
     repo_id="openai/gpt-oss-120b",
     task="text-generation",
-    provider="auto",
+    provider="auto"
 )
 
 model=ChatHuggingFace(llm=llm)
 
-# chat_history=[
-#     SystemMessage(content="you are a ai agent that is build for generating the feeedback for the product generate both positive and negative feedback ")
-
-# ]
 
 prompt=PromptTemplate(
-    template="write a feedback for the product \n {product} \n do not write both at once if you are writing a positive then write only positive and if you are writing negative then write onlhy a negative feedback bsdk kabhi negative feedback bhi de de hmesa bss positive hi de rha hai tu thoda s bahan ka loda hai kya kabhi negative de kabhi positive de bss consistently ek feedback hi kyo  de rha hai ",
+    template="generate the feedbak for the producct \n {product} \n the probability of generation negative feedback is 0.5 and the probability for generating the feedback for the negative feeedback is 0.5",
     input_Variables=["product"]
+)
+prompt1=PromptTemplate(
+    template="clasify wether the feedback  is positive or negative here is the feedback \n {feedback}",
+    input_Variables=["feedback"]
 )
 
 parser=StrOutputParser()
 
 
 
-chain=prompt | model | parser
+chain=prompt | model | parser | prompt1 | model | parser
 
 result=chain.invoke({"laptop"})
 
